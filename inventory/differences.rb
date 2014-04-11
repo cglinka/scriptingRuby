0e7b9d488e3edc45a5c7206d18f27d7e82aabefd
Clare Glinka <glinka.cb@gmail.com>
2014-04-10 23:16:35 -0500

    Excercise 3, add number of files added, deleted, unchanged.

--- inventory/differences.rb
+++ inventory/differences.rb
@@ -1,9 +1,20 @@
 old_inventory = File.open('old-inventory.txt').readlines
 new_inventory = File.open('new-inventory.txt').readlines
 
+num_files_added   = (new_inventory - old_inventory).length
+num_files_deleted = (old_inventory - new_inventory).length
+num_files_unchanged = (new_inventory & old_inventory).length
+# Other way to calculate the number of unchanged.
+# old_inventory.length - num_files_deleted
+
+puts "#{num_files_added} file(s) were added."
 puts "The following files have been added:"
 puts new_inventory - old_inventory
 
 puts ""
+puts "#{num_files_deleted} file(s) were deleted."
 puts "The following files have been deleted:"
 puts old_inventory - new_inventory
+
+puts ""
+puts "#{num_files_unchanged} file(s) were unchanged."
